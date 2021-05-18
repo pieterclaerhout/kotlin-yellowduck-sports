1218a6ff9ba0538040628a90dfd9254ef506e9d5
Pieter Claerhout <pieter@yellowduck.be>
2021-05-18 08:13:17 +0200

    Added the calculation to get the distance between two points

--- src/test/kotlin/be/yellowduck/sports/gpx/TrackPointTests.kt
+++ src/test/kotlin/be/yellowduck/sports/gpx/TrackPointTests.kt
@@ -13,14 +13,14 @@ class TrackPointTests {
     fun newTrackPointConstructor() {
 
         var point = TrackPoint(
-            lon = 16.34528,
             lat = 48.1969,
+            lon = 16.34528,
             ele = 2.6,
             time = now
         )
 
-        assertThat(point.lon).isEqualTo(16.34528)
         assertThat(point.lat).isEqualTo(48.1969)
+        assertThat(point.lon).isEqualTo(16.34528)
         assertThat(point.ele).isEqualTo(2.6)
         assertThat(point.time).isEqualTo(now)
 
@@ -30,15 +30,26 @@ class TrackPointTests {
     fun newTrackPointSetters() {
 
         var point = TrackPoint()
-        point.lon = 16.34528
         point.lat = 48.1969
+        point.lon = 16.34528
         point.ele = 2.6
         point.time = now
 
-        assertThat(point.lon).isEqualTo(16.34528)
         assertThat(point.lat).isEqualTo(48.1969)
+        assertThat(point.lon).isEqualTo(16.34528)
         assertThat(point.ele).isEqualTo(2.6)
         assertThat(point.time).isEqualTo(now)
+
+    }
+
+    @Test
+    fun distanceTo() {
+
+        var point1 = TrackPoint(lat = 12.5, lon = 43.5)
+        var point2 = TrackPoint(lat = 13.5, lon = 45.5)
+
+        val distance = point1.distanceTo(point2)
+        assertThat(distance).isEqualTo(243551.33090329584)
 
     }
 
